ef5516acbba318ab1577c9e421ef55c5e8c99afb
gayan-isuru <gayanisuru777@gmail.com>
2025-09-26 13:07:21 +0530

    work with calenders method 2

--- src/test/java/WorkWithCalender.java
+++ src/test/java/WorkWithCalender.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class WorkWithCalender {
 
     WebDriver driver;
@@ -15,14 +17,47 @@ public class WorkWithCalender {
 
     }
     @Test
+    //method 1
     public void workWithCalenderExample1() {
-        driver.get("https://jqueryui.com/datepicker/");
+//        driver.get("https://jqueryui.com/datepicker/");
 
+//        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame']")));
+//        WebElement datePicker = driver.findElement(By.xpath("//input[@id='datepicker']"));
+//        datePicker.click();
+//
+//        datePicker.sendKeys("10/10/2025");
+
+    }
+
+    @Test
+    //method 2
+    public void workWithCalenderExample2() {
+        driver.get("https://jqueryui.com/datepicker/");
         driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame']")));
         WebElement datePicker = driver.findElement(By.xpath("//input[@id='datepicker']"));
         datePicker.click();
 
-        datePicker.sendKeys("10/10/2025");
+        while(true){
+
+        String actualMonth = driver.findElement(By.xpath("//span[@class='ui-datepicker-month']")).getText();
+        String actualYear = driver.findElement(By.xpath("//span[@class='ui-datepicker-year']")).getText();
+
+        if(actualMonth.equals("October") && actualYear.equals("2025")) {
+            break;
+        }
+        else{
+            driver.findElement(By.xpath("//a[@title='Next']")).click();
+        }
+
+        }
+        List<WebElement>  selectDate = driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']/tbody/tr/td"));
+        for (WebElement element : selectDate) {
+            if(element.getText().equals("10")){
+                element.click();
+                break;
+            }
+        }
 
     }
 }
+
