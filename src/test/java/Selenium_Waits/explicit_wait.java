fa749e762d4a52adecbcbfa3852816ca974fd9c4
gayan-isuru <gayanisuru777@gmail.com>
2025-09-28 16:53:24 +0530

    Work with selenium waits and use explicit_wait
     get new btn text using wait.until(ExpectedConditions.presenceOfElementLocated(newBtnLocator));

--- src/test/java/Selenium_Waits/explicit_wait.java
+++ src/test/java/Selenium_Waits/explicit_wait.java
@@ -1,4 +1,47 @@
 package Selenium_Waits;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class explicit_wait {
+
+
+WebDriver driver;
+@BeforeMethod
+    public void beforeMethod()
+    {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.leafground.com/waits.xhtml");
+    }
+
+    @Test
+    public void testexplicit_wait()
+    {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        //select click button
+        WebElement selectBtn = driver.findElement(By.xpath("//button[@id='j_idt87:j_idt89']//span[@class='ui-button-text ui-c']"));
+        selectBtn.click();
+
+        //Thread.sleep(10000);
+
+
+        By newBtnLocator = By.xpath("//button[@id='j_idt87:j_idt90']/span[@class='ui-button-text ui-c']");
+
+        WebElement NewBtnText = wait.until(ExpectedConditions.presenceOfElementLocated(newBtnLocator));
+        String newBtnText =NewBtnText.getText();
+        System.out.println("New Button text is :"+newBtnText);
+    }
+
+
 }
+
